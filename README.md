920cafb00bdfcf84ec1d169770debaf16438f052
Mark Avenius <mark.avenius@skykick.com>
2025-04-24 21:44:08 -0600

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # litime_mppt_esphome
 ESPHome configuration for LiTime MPPT controllers with bluetooth (BLE)
+
+### Usage
+ - litime_solar_mppt.h should not need to be modified in order to get the default functionality out of this project.
+ - In litime_solar_mppt.yaml, update the values in `substitutions` to match your needs (at least the liTimeMacAddress will need to be updated to match your device. You can determine your device's MAC by using a BLE scanner on Android or iOS.)
+
+### Supported device(s)
+This has been tested with a BT-LTMPPT4860 charge controller.
+
+### Getting started with ESPHome
+If you don't yet have any experience with ESPHome, I recommend looking here for guidance: https://esphome.io/guides/getting_started_command_line
+I use the command line to compile and upload my configurations to my ESP32 board(s) and therefore don't have any experience with the ESPHome web interface (glancing through the docs, I don't know how to use a header file like `litime_solar_mppt.h` that is used in this project, so the CLI is probably your best bet.)
+Here are instructions to install ESPHome manually (which is what I did.) https://esphome.io/guides/installing_esphome
+
+Once that is done, you can run commands such as these from the root of this project:
+`esphome compile litime_solar_mppt.yaml` # (compiles without uploading, so you can test your changes without needing a board connected)
+`esphome run litime_solar_mppt.yaml` # (compiles and uploads to a connected board)
+
+### Disclaimer
+
+This is not an official library endorsed by the device manufacturer. LiTime and all other trademarks in this repo are the property of their respective owners and their use herein does not imply any sponsorship or endorsement. Further, all code contained herein is provided "as is" without guarantees of performance or suitability; use it at your own risk.
---
+++ litime_solar_mppt.h
@@ -0,0 +1,72 @@
+#pragma once
+using namespace std;
+
+void HandleResponseData(const vector<uint8_t>& data) {
+  if (data.size() < 42) {
+    ESP_LOGW("mppt", "Received data too short: %d", data.size());
+    return;
+  }
+
+  // Parse the data based on the format from your data streams
+  // Example format: 0103260064008f05c900d31bff008f000200000321036804ba0000800400000003000017e000000000a884
+
+  if (data[0] != 0x01 || data[1] != 0x03) {
+    ESP_LOGW("mppt", "Invalid header");
+    return;
+  }
+
+  // Extract values based on our understanding of the data format
+  // Convert the raw bytes to actual values
+
+  // Battery data
+  uint16_t raw_batt_voltage = (data[5] << 8) | data[6];
+  id(battery_voltage).publish_state(raw_batt_voltage * 0.1f);
+
+  uint16_t raw_batt_current = (data[7] << 8) | data[8];
+  id(battery_current).publish_state(raw_batt_current * 0.01f);
+
+  uint16_t raw_batt_power = (data[9] << 8) | data[10];
+  id(battery_power).publish_state(raw_batt_power);
+
+  // Controller temperature
+  id(controller_temp).publish_state(data[11]);
+
+  // data[12] == 'ff' - spacer?
+  
+  // Load data (if enabled)
+  uint16_t raw_load_voltage = (data[13] << 8) | data[14];
+  id(load_voltage).publish_state(raw_load_voltage * 0.1f);
+
+  uint16_t raw_load_current = (data[15] << 8) | data[16];
+  id(load_current).publish_state(raw_load_current * 0.01f);
+
+  uint16_t raw_load_power = (data[17] << 8) | data[18];
+  id(load_power).publish_state(raw_load_power);
+
+  // PV input
+  uint16_t raw_pv_voltage = (data[19] << 8) | data[20];
+  id(pv_voltage).publish_state(raw_pv_voltage * 0.1f);
+
+  // Daily max power
+  uint16_t raw_max_power = (data[21] << 8) | data[22];
+  id(max_charge_power).publish_state(raw_max_power);
+
+  // Energy charged today
+  uint16_t raw_energy_today = (data[23] << 8) | data[24];
+  id(energy_today).publish_state(raw_energy_today);
+
+  // Controller mode
+  //todo: convert
+  // id(controller_mode).publish_state(data[26]);
+
+  // Running days
+  uint16_t raw_days = (data[31] << 8) | data[32];
+  id(running_days).publish_state(raw_days);
+
+  // Total energy charged
+  uint16_t raw_total_energy = (data[35] << 8) | data[36];
+  id(total_energy).publish_state(raw_total_energy);
+
+  ESP_LOGI("mppt", "Data parsed successfully: Battery %.1fV, %.2fA, %dW",
+          raw_batt_voltage * 0.1f, raw_batt_current * 0.1f, raw_batt_power);
+}
